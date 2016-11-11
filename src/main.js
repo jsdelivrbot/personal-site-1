5b01ad8831ac11c9fcae43d462e103b7863d895d
anniehck <anniehckim@gmail.com>
2016-11-11 12:37:10 -0500

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './main.scss';
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>BOOOOOO YA</h1>,
+    document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    path: './src/main.js'
+  },
+  output: {
+    path: './build',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loaders: ["style", "css", "sass"]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './build',
+    inline: true,
+    hot: true
+  }
+}
+
+if (process.env.NODE_ENV === 'production') {
+  delete config.devtool;
+  var webpack = require('webpack');
+  config.plugins = [
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' })
+  ];
+}

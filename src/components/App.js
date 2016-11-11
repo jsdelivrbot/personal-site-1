07395d2e91d5ad0bd32b5789aba3b0c96f84ddf2
anniehck <anniehckim@gmail.com>
2016-11-11 13:46:48 -0500

    Style navigation

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <ul role="nav">
+        <ul role="nav" id="main-nav">
           <li><NavLink to="/" onlyActiveOnIndex={true}>Home</NavLink></li>
           <li><NavLink to="/about">About</NavLink></li>
           <li><NavLink to="/projects">Projects</NavLink></li>
